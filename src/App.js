07cc779d267d01fffbe76ac3f84b3819a15fc84a
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2022-01-18 23:52:20 +0530

    face detection on hardcoded image

--- src/App.js
+++ src/App.js
@@ -14,7 +14,21 @@ const App = () => {
      .withFaceExpressions();
     //  .withAgeAndGender();
 
-     console.log(detections)
+    //  console.log(detections)
+
+    canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(imgRef.current);
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 940,
+      height: 650,
+    });
+
+    const resized = faceapi.resizeResults(detections, {
+      width: 940,
+      height: 650,
+    })
+    faceapi.draw.drawDetections(canvasRef.current, resized)         //For drawing boxes on the image
+    faceapi.draw.drawFaceExpressions(canvasRef.current, resized)    //For writing the expressions on the image
+    faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)      //For drawing the dotted outline on the image
   }
 
   useEffect(()=>{
@@ -22,7 +36,7 @@ const App = () => {
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
         faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        // faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
         faceapi.nets.faceExpressionNet.loadFromUri("/models"),
       ])
       .then(handleImage)
@@ -39,6 +53,8 @@ const App = () => {
     ref = {imgRef}
     style={{"objectFit": "cover"}}
     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvULB4tfyXgIpY7BVOw0AnIykmXPLzlkLzsg&usqp=CAU' 
+    // src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg3ClUuZgxAzmezqlM6f4NZB7rmn3JL9uu5w&usqp=CAU'
+    // src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHpkdSWbQqFKeCU4JarGMjFtRPaci6saRQ7g&usqp=CAU'
     alt="" 
     width="940" 
     height = "650" 
